44af65ffaa889855436b4f7244ba7af456d0bb54
Ryan Honea <ryan@honea.info>
2021-07-25 14:04:01 -0500

    Added generic login.js

--- frontend/src/components/login/Login.js
+++ frontend/src/components/login/Login.js
@@ -1,9 +1,43 @@
-import { Container } from "@material-ui/core";
-import React from "react";
+import { Container, TextField } from "@material-ui/core";
+import React, { useState } from "react";
 export default function Login() {
+  // Potential refactoring???
+  //
+  // const useCreateInput = ({ type, name, label, variant }) => {
+  //   const [value, setValue] = useState("");
+  //   const input = (
+  //     <TextField
+  //       id={name}
+  //       value={value}
+  //       label={label}
+  //       variant={variant}
+  //       type={type}
+  //       onChange={(e) => setValue(e.target.value)}
+  //     />
+  //   );
+  //   return [value, input];
+  // };
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <Container>
-      <h1>Login</h1>
+      <TextField
+        id="username"
+        value={username}
+        label="Username"
+        variant="outlined"
+        type="text"
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <TextField
+        id="password"
+        value={password}
+        label="Password"
+        variant="outlined"
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
     </Container>
   );
 }
